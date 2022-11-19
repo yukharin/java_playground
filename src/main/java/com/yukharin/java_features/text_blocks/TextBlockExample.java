b13d3d42d9d70a32adcae1f99be6d33c27364269
Vladislav_Iukharin <Vladislav_Iukharin@epam.com>
2022-11-19 15:37:13 +0600

    Add example with text blocks

--- src/main/java/com/yukharin/java_features/text_blocks/TextBlockExample.java
+++ src/main/java/com/yukharin/java_features/text_blocks/TextBlockExample.java
@@ -6,13 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 public class TextBlockExample {
 
     public static void main(String[] args) {
-        var query = """ 
+        var multipleLineText = """ 
                 SELECT * FROM person p
                 JOIN book b ON p.id = b.person_id
                 where p.age > 18
                 """;
 
-        log.info("Hello world my first text block: [{}]", query);
+        var oneLineText = """
+                SELECT * FROM person p\
+                JOIN book b ON p.id = b.person_id\
+                where p.age > 18
+                """;
+
+        log.info("Multiple line text: [{}]", multipleLineText);
+        log.info("One line text: [{}]", oneLineText);
     }
 
 }
