9061cb92f375cbe833bb0ef15ed0d8736d9e2154
Vladislav_Iukharin <Vladislav_Iukharin@epam.com>
2022-11-19 15:06:11 +0600

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "com.yukharin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val lombokVersion = "1.18.24"
+
+    implementation("org.springframework.boot:spring-boot-starter-logging:2.7.5")
+    compileOnly("org.projectlombok", "lombok", lombokVersion)
+    annotationProcessor("org.projectlombok", "lombok", lombokVersion)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
